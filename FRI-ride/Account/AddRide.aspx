736baf9a92e7d5b2982ab705293c64a291541498
gapus97 <Planche1997>
2020-01-07 21:29:25 +0100

    Add map that displays starting point and end point path, to make it work insert API_KEY in file addRide.aspx

--- FRI-ride/Account/AddRide.aspx
+++ FRI-ride/Account/AddRide.aspx
@@ -1,15 +1,15 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="AddRide.aspx.cs" Inherits="Account_AddRide" Async="true" %>
 
 
-
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+   
     <h2>Add ride</h2>
     <p class="text-danger">
         <asp:Literal runat="server" ID="ErrorMessage" />
     </p>
 
-
     <div class="form-horizontal">
+ 
         <hr />
         <asp:ValidationSummary runat="server" CssClass="text-danger" />
         <div class="form-group">
@@ -40,11 +40,12 @@
 
         <div class="form-group">
             <div class="col-md-offset-2 col-md-10">
-                <asp:Button runat="server" OnClick="AddRide_Click" Text="Add ride" CssClass="btn btn-default" />
+                <asp:Button runat="server" OnClick="AddRide_Click" Text="Add ride" CssClass="btn btn-default"  async defer/>
             </div>
         </div>
+        <input type="button" id="routebtn" value="route" />
+          <div id="map-canvas" style="height:800px;width: 800px;margin: 0px;padding: 0px"></div>      
     </div>
-
-
+    <script src="../Scripts/maps.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=mapLocation&libraries=places" async defer></script>
 </asp:Content>
-
---
+++ FRI-ride/Scripts/maps.js
@@ -0,0 +1,70 @@
+﻿var address;
+var lat;
+var lon;
+function mapLocation() {
+    //console.log("Lat:" + address.geometry.location.A + " lon:" + address.geometry.location.F);
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+    address = new google.maps.places.Autocomplete(document.getElementById('MainContent_Start_location'));
+
+    function initialize() {
+        console.log("111");
+
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var ljubljana = new google.maps.LatLng(46.0569465, 14.505751499999974);
+        var mapOptions = {
+            zoom: 7,
+            center: ljubljana
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        directionsDisplay.setMap(map);
+        google.maps.event.addDomListener(document.getElementById('routebtn'), 'click', calcRoute);
+
+    }
+
+    function calcRoute() {
+        place = address.getPlace();
+        lat = place.geometry.location.lat();
+        lon = place.geometry.location.lng();
+        console.log(lat + "      " + lon);
+        var start = new google.maps.LatLng(lat, lon);
+        //var end = new google.maps.LatLng(38.334818, -181.884886);
+        var end = new google.maps.LatLng(46.0501557, 14.468920499999967);
+        /*
+var startMarker = new google.maps.Marker({
+            position: start,
+            map: map,
+            draggable: true
+        });
+        var endMarker = new google.maps.Marker({
+            position: end,
+            map: map,
+            draggable: true
+        });
+*/
+        var bounds = new google.maps.LatLngBounds();
+
+
+        bounds.extend(start);
+        bounds.extend(end);
+        map.fitBounds(bounds);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap(map);
+            } else {
+                alert("Directions Request from " + start.toUrlValue(6) + " to " + end.toUrlValue(6) + " failed: " + status);
+            }
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
